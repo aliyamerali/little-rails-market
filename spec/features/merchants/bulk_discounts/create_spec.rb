84b0a007aa1ba9170244e9a2ee1a8b47e6d890b4
Aliya Merali <merali4@gmail.com>
2021-06-12 09:33:41 -0600

    Add test for form fields

--- spec/features/merchants/bulk_discounts/create_spec.rb
+++ spec/features/merchants/bulk_discounts/create_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Bulk Discount create', type: :feature do
+  before :each do
+    @merchant1 = FactoryBot.create(:merchant)
+    @merchant2 = FactoryBot.create(:merchant)
 
+    visit new_merchant_bulk_discount_path(@merchant.id)
+  end
+
+  it 'shows a form to create a new discount' do
+    expect(page).to have_field("Percentage")
+    expect(page).to have_field("Quantity Threshold")
+  end
+
+  it 'upon submission, creates a new discount for the merchant' 
+
+  it 'redirects to the merchants discount index, showing new discount'
+
+  it 'does not add the discount to any other merchant'
 
 end
